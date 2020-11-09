4658b933b2734451ccea021e25a26708221ca52b
Conrad Sachweh <conrad@csachweh.de>
2020-11-09 08:22:11 +0100

    no single letter variables

--- hnarchive.py
+++ hnarchive.py
@@ -59,8 +59,8 @@ def int_or_none(x):
 
 # API ##############################################################################################
 
-s = requests.Session()
-s.headers.update(HEADERS)
+session = requests.Session()
+session.headers.update(HEADERS)
 
 def get(url, retries=1):
     start_time = time.time()
@@ -69,7 +69,7 @@ def get(url, retries=1):
     while retries > 0:
         log.loud(url)
         try:
-            response = s.get(url, headers=HEADERS, timeout=2)
+            response = session.get(url, timeout=2)
             response.raise_for_status()
             break
         except requests.exceptions.HTTPError as exc:
